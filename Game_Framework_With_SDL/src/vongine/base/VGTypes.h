b5a50bb94c858159c91dd1257011deb65a0dc581
Ignacio Pascual <vonflaken@gmail.com>
2016-10-28 00:59:47 +0200

    Add types header with some custom primitives.

---
+++ Game_Framework_With_SDL/src/vongine/base/VGTypes.h
@@ -0,0 +1,13 @@
+#ifndef __VGTYPES_H__
+#define __VGTYPES_H__
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long int64;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
+#endif // __VGTYPES_H__
