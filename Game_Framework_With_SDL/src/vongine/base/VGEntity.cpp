e4d2b8c20c88f347dbef2e70d85f6ca2aa0df7c7
Ignacio Pascual <vonflaken@gmail.com>
2016-11-04 22:23:35 +0100

    Add Entity class, base class for all objects in scene.

---
+++ Game_Framework_With_SDL/src/vongine/base/VGEntity.cpp
@@ -0,0 +1,83 @@
+#include "VGEntity.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+NS_VG_BEGIN
+
+std::shared_ptr<Entity> Entity::Create()
+{
+	return Entity::Create(glm::vec3(0.f, 0.f, 0.f));
+}
+
+std::shared_ptr<Entity> Entity::Create(const glm::vec3& position)
+{
+	std::shared_ptr<Entity> rval = std::make_shared<Entity>();
+	return rval;
+}
+
+Entity::Entity()
+: _position(0.f, 0.f, 0.f)
+, _eulerAngles(0.f, 0.f, 0.f)
+, _scale(1.f, 1.f, 1.f)
+, _transform() // identity
+, _stateFlags(0)
+{
+}
+
+void Entity::Prepare(const glm::mat4& parentTransform, const uint32 parentFlags)
+{
+	uint32 mergedFlags = parentFlags | _stateFlags;
+
+	if (mergedFlags & FLAGS_TRANSFORM_DIRTY)
+	{
+		// Update transform
+
+		_transform = parentTransform * GetToParentTranform();
+	}
+
+	// Prepare children
+	for (auto it = _children.begin(); it != _children.end(); it++)
+	{		
+		(*it)->Prepare(_transform, _stateFlags);
+	}
+
+	_stateFlags |= ~FLAGS_TRANSFORM_DIRTY; // Set transform bit as updated
+}
+
+const glm::mat4& Entity::GetToParentTranform() const
+{
+	glm::mat4 mat;
+	
+	glm::translate(mat, _position); // translate
+	
+	// rotate
+	glm::rotate(mat, _eulerAngles.x, glm::vec3(1.f, 0.f, 0.f));
+	glm::rotate(mat, _eulerAngles.y, glm::vec3(0.f, 1.f, 0.f));
+	glm::rotate(mat, _eulerAngles.z, glm::vec3(0.f, 0.f, 1.f));
+
+	glm::scale(mat, _scale); // scale
+
+	return mat;
+}
+
+void Entity::SetPosition(const glm::vec3& position)
+{
+	_stateFlags |= FLAGS_TRANSFORM_DIRTY;
+
+	_position = position; 
+}
+
+void Entity::SetEulerAngles(const glm::vec3& eulerAngles)
+{ 
+	_stateFlags |= FLAGS_TRANSFORM_DIRTY;
+
+	_eulerAngles = eulerAngles; 
+}
+
+void Entity::SetScale(const glm::vec3& scale)
+{ 
+	_stateFlags |= FLAGS_TRANSFORM_DIRTY;
+
+	_scale = scale; 
+}
+
+NS_VG_END
---
+++ Game_Framework_With_SDL/src/vongine/base/VGEntity.h
@@ -0,0 +1,52 @@
+#ifndef __VGENTITY_H__
+#define __VGENTITY_H__
+
+#include "VGMacros.h"
+#include "glm/glm.hpp"
+#include "VGTypes.h"
+#include <memory>
+#include <vector>
+
+NS_VG_BEGIN
+
+class __declspec(dllexport) Entity
+{
+	enum {
+		FLAGS_TRANSFORM_DIRTY = (1 << 0)
+	};
+
+public:
+	static std::shared_ptr<Entity> Create();
+	static std::shared_ptr<Entity> Create(const glm::vec3& position);
+
+	Entity();
+	virtual ~Entity() {}
+
+	void SetPosition(const glm::vec3& position);
+	const glm::vec3& GetPosition() const { return _position; }
+
+	void SetEulerAngles(const glm::vec3& eulerAngles);
+	const glm::vec3& GetEulerAngles() const { return _eulerAngles; }
+
+	void Entity::SetScale(const glm::vec3& scale);
+	const glm::vec3& GetScale() const { return _scale; }
+
+	virtual void Prepare(const glm::mat4& parentTransform, const uint32 parentFlags); // Update transform If needed
+	virtual void Draw() {}
+
+	virtual const glm::mat4& GetToParentTranform() const;
+
+protected:
+	glm::vec3 _position;
+	glm::vec3 _eulerAngles; // Rotation in euler angles
+	glm::vec3 _scale;
+	glm::mat4 _transform;
+
+	uint32 _stateFlags;
+
+	std::vector<std::shared_ptr<Entity>> _children;
+};
+
+NS_VG_END
+
+#endif // __VGENTITY_H__
