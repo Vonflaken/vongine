3f84273faa131d50831c8b96cc15c91e53f9666a
Ignacio Pascual <vonflaken@gmail.com>
2016-11-11 03:02:36 +0100

    - Fix typo 'GetToParentTranform'. - Refactor when matrices get updated. - Refactor 'Prepare' function. - Remove '_stateFlags' member var. - Add functions that handle transform matrices calculation. - Add 'GetWorldPosition' function.

--- Game_Framework_With_SDL/src/vongine/base/VGEntity.cpp
+++ Game_Framework_With_SDL/src/vongine/base/VGEntity.cpp
@@ -18,68 +18,116 @@ Entity::Entity()
 : _position(0.f, 0.f, 0.f)
 , _eulerAngles(0.f, 0.f, 0.f)
 , _scale(1.f, 1.f, 1.f)
-, _transform() // identity
-, _stateFlags(0)
-, _parent(nullptr)
+, _transformMatrix() // identity
+, _modelMatrix()
+, _transformUpdated(true)
+, _transformDirty(true)
 {
 }
 
 void Entity::Prepare(const glm::mat4& parentTransform, const uint32 parentFlags)
 {
-	uint32 mergedFlags = parentFlags | _stateFlags;
-
-	if (mergedFlags & FLAGS_TRANSFORM_DIRTY)
-	{
-		// Update transform
-
-		_transform = parentTransform * GetToParentTranform();
-		_stateFlags |= FLAGS_TRANSFORM_DIRTY; // Set dirty for propagate transform update to childs
-	}
+	uint32 flags = ProcessParentFlags(parentTransform, parentFlags);
 
 	// Prepare children
 	for (auto it = _children.begin(); it != _children.end(); it++)
 	{		
-		(*it)->Prepare(_transform, _stateFlags);
+		(*it)->Prepare(_modelMatrix, flags);
 	}
-
-	_stateFlags |= ~FLAGS_TRANSFORM_DIRTY; // Set transform bit as updated
 }
 
-const glm::mat4 Entity::GetToParentTranform() const
+uint32 Entity::ProcessParentFlags(const glm::mat4& parentTransform, const uint32 parentFlags)
 {
-	glm::mat4 mat;
-	
-	glm::translate(mat, _position); // translate
-	
-	// rotate
-	glm::rotate(mat, _eulerAngles.x, glm::vec3(1.f, 0.f, 0.f));
-	glm::rotate(mat, _eulerAngles.y, glm::vec3(0.f, 1.f, 0.f));
-	glm::rotate(mat, _eulerAngles.z, glm::vec3(0.f, 0.f, 1.f));
+	uint32 flags = parentFlags;
 
-	glm::scale(mat, _scale); // scale
+	// Set ON transform dirty bit If position, rotation, scale...have changed this frame
+	flags |= (_transformUpdated ? FLAGS_TRANSFORM_DIRTY : 0);
 
-	return mat;
+	if (flags & FLAGS_TRANSFORM_DIRTY)
+	{
+		// Update Model
+		_modelMatrix = CalculateModel(parentTransform);
+	}
+
+	_transformUpdated = false;
+
+	return flags;
+}
+
+const glm::mat4& Entity::GetToParentTransform()
+{
+	if (_transformDirty)
+	{
+		glm::mat4 mat;
+
+		glm::translate(mat, _position); // translate
+
+		// rotate
+		glm::rotate(mat, _eulerAngles.x, glm::vec3(1.f, 0.f, 0.f));
+		glm::rotate(mat, _eulerAngles.y, glm::vec3(0.f, 1.f, 0.f));
+		glm::rotate(mat, _eulerAngles.z, glm::vec3(0.f, 0.f, 1.f));
+
+		glm::scale(mat, _scale); // scale
+
+		_transformMatrix = mat;
+	}
+
+	_transformDirty = false;
+
+	return _transformMatrix;
+}
+
+const glm::mat4 Entity::GetToParentTransform(const std::shared_ptr<Entity> ancestor)
+{
+	glm::mat4 transform(GetToParentTransform());
+
+	for (auto parent = _parent.lock(); parent && parent != ancestor; parent = parent->GetParent().lock())
+	{
+		transform = parent->GetToParentTransform() * transform;
+	}
+
+	return transform;
+}
+
+const glm::mat4 Entity::CalculateModel(const glm::mat4& parentTransform)
+{
+	return parentTransform * GetToParentTransform();
+}
+
+const glm::vec3 Entity::GetWorldPosition()
+{
+	glm::vec3 worldPos;
+	if (!_transformUpdated)
+	{
+		worldPos = _modelMatrix * glm::vec4(glm::vec3(0.f), 1.f);
+	}
+	else
+	{
+		worldPos = GetToParentTransform(nullptr) * glm::vec4(glm::vec3(0.f), 1.f);
+	}
+
+	return worldPos;
 }
 
 void Entity::SetPosition(const glm::vec3& position)
 {
-	_stateFlags |= FLAGS_TRANSFORM_DIRTY;
-
 	_position = position; 
+
+	_transformUpdated = _transformDirty = true;
 }
 
 void Entity::SetEulerAngles(const glm::vec3& eulerAngles)
 { 
-	_stateFlags |= FLAGS_TRANSFORM_DIRTY;
-
 	_eulerAngles = eulerAngles; 
+
+	_transformUpdated = _transformDirty = true;
 }
 
 void Entity::SetScale(const glm::vec3& scale)
-{ 
-	_stateFlags |= FLAGS_TRANSFORM_DIRTY;
-
+{
 	_scale = scale; 
+
+	_transformUpdated = _transformDirty = true;
 }
 
 void Entity::AddChild(const std::shared_ptr<Entity> entity)
@@ -88,8 +136,6 @@ void Entity::AddChild(const std::shared_ptr<Entity> entity)
 	_children.push_back(entity);
 	// Set 'entity' parent
 	entity->SetParent(std::shared_ptr<Entity>(this));
-	// Will update transform, now transform must be relative to parent
-	AddFlags(FLAGS_TRANSFORM_DIRTY);
 }
 
 void Entity::DetachChild(const std::shared_ptr<Entity> entity)
@@ -110,11 +156,9 @@ void Entity::DetachChild(const std::shared_ptr<Entity> entity)
 void Entity::SetParent(const std::shared_ptr<Entity> entity)
 {
 	_parent = entity;
-}
 
-void Entity::AddFlags(const uint32 flags)
-{
-	_stateFlags |= flags;
+	_transformUpdated = _transformDirty = true;
+
 }
 
 NS_VG_END
--- Game_Framework_With_SDL/src/vongine/base/VGEntity.h
+++ Game_Framework_With_SDL/src/vongine/base/VGEntity.h
@@ -25,13 +25,11 @@ public:
 	void AddChild(const std::shared_ptr<Entity> entity);
 	void DetachChild(const std::shared_ptr<Entity> entity);
 	void SetParent(const std::shared_ptr<Entity> entity);
-
-	// Merge 'flags' with entity flags
-	void AddFlags(const uint32 flags);
+	const std::weak_ptr<Entity> GetParent() const { return _parent; }
 
 	void SetPosition(const glm::vec3& position);
 	const glm::vec3& GetPosition() const { return _position; }
-	const glm::vec3 GetWorldPosition() const;
+	const glm::vec3 GetWorldPosition();
 
 	void SetEulerAngles(const glm::vec3& eulerAngles);
 	const glm::vec3& GetEulerAngles() const { return _eulerAngles; }
@@ -39,20 +37,28 @@ public:
 	void Entity::SetScale(const glm::vec3& scale);
 	const glm::vec3& GetScale() const { return _scale; }
 
-	virtual void Prepare(const glm::mat4& parentTransform, const uint32 parentFlags); // Update transform If needed
+	virtual void Prepare(const glm::mat4& parentTransform, const uint32 parentFlags); // Update entity state (transform, etc.) If needed and get ready for be drawn
 	virtual void Draw() {}
 
 	// Returns transform relative to parent
-	const glm::mat4 GetToParentTranform() const;
+	const glm::mat4& GetToParentTransform();
+	// Returns transform relative to certain parent
+	const glm::mat4 GetToParentTransform(const std::shared_ptr<Entity> ancestor);
+	// Returns Transform matrix (aka Model)
+	const glm::mat4 CalculateModel(const glm::mat4& parentTransform);
 	
+protected:
+	uint32 ProcessParentFlags(const glm::mat4& parentTransform, const uint32 parentFlags); // Update parent dependent state If needed
 
 protected:
 	glm::vec3 _position;
 	glm::vec3 _eulerAngles; // Rotation in euler angles
 	glm::vec3 _scale;
-	glm::mat4 _transform;
+	glm::mat4 _transformMatrix; // Entity to parent transform
+	glm::mat4 _modelMatrix; // Model matrix
 
-	uint32 _stateFlags;
+	bool _transformDirty;
+	bool _transformUpdated; // Tell whether transform was updated or not in current frame
 
 	std::weak_ptr<Entity> _parent;
 	std::vector<std::shared_ptr<Entity>> _children;
