3c88382d1f333b4cc6b3c8b58e4094e3b7dc5bc1
Ignacio Pascual <vonflaken@gmail.com>
2016-11-15 18:02:23 +0100

    - Remove ProccessParentsFlags override. - Now update of view and viewProjection matrices is on demand. - Set projection matrix through both InitOrtho or InitPerspective.

--- Game_Framework_With_SDL/src/vongine/rendering/VGCamera.cpp
+++ Game_Framework_With_SDL/src/vongine/rendering/VGCamera.cpp
@@ -6,36 +6,87 @@ NS_VG_BEGIN
 std::shared_ptr<Camera> Camera::CreateOrtho(const float left, const float right, const float bottom, const float top, const float near, const float far)
 {
 	std::shared_ptr<Camera> camera = std::make_shared<Camera>();
-	glm::mat4 projMat = glm::ortho(left, right, bottom, top, near, far);
-	camera->SetProjectionMatrix(projMat);
+	camera->InitOrtho(left, right, bottom, top, near, far);
 	return camera;
 }
 
 std::shared_ptr<Camera> Camera::CreatePerspective(const float fov, const float aspectRatio, const float near, const float far)
 {
 	std::shared_ptr<Camera> camera = std::make_shared<Camera>();
-	glm::mat4 projMat = glm::perspective(fov, aspectRatio, near, far);
-	camera->SetProjectionMatrix(projMat);
+	camera->InitPerspective(fov, aspectRatio, near, far);
 	return camera;
 }
 
-uint32 Camera::ProcessParentFlags(const glm::mat4& parentTransform, const uint32 parentFlags)
+Camera::Camera()
+: _viewMatrixDirty(true)
 {
-	uint32 processedFlags = Entity::ProcessParentFlags(parentTransform, parentFlags);
+}
 
-	if (processedFlags & FLAGS_TRANSFORM_DIRTY)
+void Camera::InitPerspective(const float fov, const float aspectRatio, const float near, const float far)
+{
+	_projectionMatrix = glm::perspective(fov, aspectRatio, near, far);
+	_viewProjectionMatrixDirty = true;
+}
+
+void Camera::InitOrtho(const float left, const float right, const float bottom, const float top, const float near, const float far)
+{
+	_projectionMatrix = glm::ortho(left, right, bottom, top, near, far);
+	_viewProjectionMatrixDirty = true;
+}
+
+const glm::mat4& Camera::GetViewProjectionMatrix()
+{
+	glm::mat4 view = GetViewMatrix();
+	if (_viewProjectionMatrixDirty)
+	{
+		_viewProjectionMatrix = _projectionMatrix * view;
+
+		_viewProjectionMatrixDirty = false;
+	}
+
+	return _viewProjectionMatrix;
+}
+
+const glm::mat4& Camera::GetViewMatrix()
+{
+	if (_viewMatrixDirty)
 	{
 		// Update View matrix
 		glm::vec3 target = _position + GetTransformForwardOrientation();
-		UpdateViewMatrix(target);
+		_viewMatrix = glm::lookAt(GetEye(), target, glm::vec3(0.f, 1.f, 0.f)); // Set
+
+		_viewMatrixDirty = false;
 	}
 
-	return processedFlags;
+	return _viewMatrix;
 }
 
-void Camera::UpdateViewMatrix(const glm::vec3& target)
+void Camera::SetParent(const std::shared_ptr<Entity> entity)
 {
-	_viewMatrix = glm::lookAt(GetEye(), target, glm::vec3(0.f, 1.f, 0.f));
+	Entity::SetParent(entity);
+
+	_viewProjectionMatrixDirty = _viewMatrixDirty = true;
+}
+
+void Camera::SetPosition(const glm::vec3& position)
+{
+	Entity::SetPosition(position);
+
+	_viewProjectionMatrixDirty = _viewMatrixDirty = true;
+}
+
+void Camera::SetEulerAngles(const glm::vec3& eulerAngles)
+{
+	Entity::SetEulerAngles(eulerAngles);
+
+	_viewProjectionMatrixDirty = _viewMatrixDirty = true;
+}
+
+void Camera::SetScale(const glm::vec3& scale)
+{
+	Entity::SetScale(scale);
+
+	_viewProjectionMatrixDirty = _viewMatrixDirty = true;
 }
 
 NS_VG_END
--- Game_Framework_With_SDL/src/vongine/rendering/VGCamera.h
+++ Game_Framework_With_SDL/src/vongine/rendering/VGCamera.h
@@ -12,22 +12,32 @@ public:
 	static std::shared_ptr<Camera> CreateOrtho(const float left, const float right, const float bottom, const float top, const float near, const float far);
 	static std::shared_ptr<Camera> CreatePerspective(const float fov, const float aspectRatio, const float near, const float far);
 
-	void SetProjectionMatrix(const glm::mat4& projMat) { _projectionMatrix = projMat; }
+	Camera();
+
+	void InitPerspective(const float fov, const float aspectRatio, const float near, const float far);
+	void InitOrtho(const float left, const float right, const float bottom, const float top, const float near, const float far);
+
+	virtual void SetParent(const std::shared_ptr<Entity> entity);
+
+	virtual void SetPosition(const glm::vec3& position);
+	virtual void SetEulerAngles(const glm::vec3& eulerAngles);
+	virtual void SetScale(const glm::vec3& scale);
+
 	const glm::mat4& GetProjectionMatrix() const { return _projectionMatrix; }
-	const glm::mat4 GetViewProjectionMatrix() const { return _projectionMatrix * _viewMatrix; }
+	const glm::mat4& GetViewMatrix();
+	const glm::mat4& GetViewProjectionMatrix();
 
 	const glm::vec3 GetEye() { return GetWorldPosition(); }
-
-	void UpdateViewMatrix(const glm::vec3& target);
-
-protected:
-	virtual uint32 ProcessParentFlags(const glm::mat4& parentTransform, const uint32 parentFlags);
 
 private:
 	static std::shared_ptr<Camera> s_renderingCamera;
 
 	glm::mat4 _viewMatrix;
 	glm::mat4 _projectionMatrix;
+	glm::mat4 _viewProjectionMatrix;
+
+	bool _viewMatrixDirty;
+	bool _viewProjectionMatrixDirty;
 };
 
 NS_VG_END
