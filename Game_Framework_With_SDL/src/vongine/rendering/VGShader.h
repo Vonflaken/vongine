08c2046bbeb18123e2c1e0ecf4551ce32e9ca733
Ignacio Pascual <vonflaken@gmail.com>
2017-03-16 17:09:00 +0100

    Add Shader class. Compile GLSL shaders.

---
+++ Game_Framework_With_SDL/src/vongine/rendering/VGShader.h
@@ -0,0 +1,32 @@
+#ifndef __VGSHADER_H__
+#define __VGSHADER_H__
+
+#include "VGMacros.h"
+#include "ogl/glew.h"
+
+NS_VG_BEGIN
+
+enum ShaderType
+{
+	VERTEX = GL_VERTEX_SHADER, 
+	FRAGMENT = GL_FRAGMENT_SHADER
+};
+
+class __declspec(dllexport) Shader
+{
+public:
+	Shader();
+	/// Delete shader
+	virtual ~Shader();
+
+	/// Creates and compiles shader object
+	void Compile(const char* filename, const ShaderType type);
+
+private:
+	GLuint _name;
+};
+
+NS_VG_END
+
+#endif // __VGSHADER_H__
+
