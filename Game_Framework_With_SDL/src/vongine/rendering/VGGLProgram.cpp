e1a782a44bad79f1b1f3798414ee3323407c04ea
Ignacio Pascual <vonflaken@gmail.com>
2017-03-16 20:18:09 +0100

    Add empty GLProgram sources.

---
+++ Game_Framework_With_SDL/src/vongine/rendering/VGGLProgram.cpp
@@ -0,0 +1,5 @@
+#include "VGGLProgram.h"
+
+NS_VG_BEGIN
+
+NS_VG_END
---
+++ Game_Framework_With_SDL/src/vongine/rendering/VGGLProgram.h
@@ -0,0 +1,15 @@
+#ifndef __VGGLPROGRAM_H__
+#define __VGGLPROGRAM_H__
+
+#include "VGMacros.h"
+
+NS_VG_BEGIN
+
+class __declspec(dllexport) GLProgram
+{
+
+};
+
+NS_VG_END
+
+#endif // __VGGLPROGRAM_H__
