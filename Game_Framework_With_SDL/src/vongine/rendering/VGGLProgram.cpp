2f4fe4038f9c7cd61b66d19804fc998a3aa80314
Ignacio Pascual <vonflaken@gmail.com>
2017-03-17 13:52:25 +0100

    Implement basic GLProgram class. Builds a glprogram with two shaders (vertex/fragment).

--- Game_Framework_With_SDL/src/vongine/rendering/VGGLProgram.cpp
+++ Game_Framework_With_SDL/src/vongine/rendering/VGGLProgram.cpp
@@ -1,5 +1,77 @@
 #include "VGGLProgram.h"
+#include "rendering/VGShader.h"
+#include <vector>
+#include "base/VGLogger.h"
 
 NS_VG_BEGIN
+
+GLProgram::GLProgram()
+: _program(0)
+{}
+
+GLProgram::~GLProgram()
+{
+	DeleteProgram();
+}
+
+bool GLProgram::Build(const Shader* vertexShader, const Shader* fragmentShader)
+{
+	// Get both shader identifiers
+	GLuint vertexName = vertexShader->GetName();
+	GLuint fragmentName = fragmentShader->GetName();
+
+	if (!vertexName || !fragmentName) // Early exist if one of the shaders is invalid
+		return false;
+
+	if (_program) // Early exist if program is already built
+		return true;
+
+	bool success = false; // Result of building
+
+	// Create program
+	_program = glCreateProgram();
+
+	// Attach both shaders
+	glAttachShader(_program, vertexName);
+	glAttachShader(_program, fragmentName);
+
+	// Link program
+	glLinkProgram(_program);
+
+	// Detach shaders after link
+	glDetachShader(_program, vertexName);
+	glDetachShader(_program, fragmentName);
+
+	// Handle linking errors
+	GLint isLinked = 0;
+	glGetProgramiv(_program, GL_LINK_STATUS, (int *)&isLinked); // Get link status
+	if (isLinked == GL_TRUE)
+	{
+		success = true;
+	}
+	else
+	{
+		// Linking error
+
+		GLint maxLength = 0;
+		glGetProgramiv(_program, GL_INFO_LOG_LENGTH, &maxLength); // Get info log string length
+
+		std::vector<GLchar> infoLog(maxLength);
+		glGetProgramInfoLog(_program, maxLength, &maxLength, &infoLog[0]); // Get info log string
+
+		VGLOG("%s", &infoLog[0]); // Log error info
+	}
+
+	if (!success)
+		DeleteProgram(); // Cleanup program if building failed
+
+	return success;
+}
+
+void GLProgram::DeleteProgram()
+{
+	glDeleteProgram(_program);
+	_program = 0;
+}
 
 NS_VG_END
--- Game_Framework_With_SDL/src/vongine/rendering/VGGLProgram.h
+++ Game_Framework_With_SDL/src/vongine/rendering/VGGLProgram.h
@@ -2,12 +2,28 @@
 #define __VGGLPROGRAM_H__
 
 #include "VGMacros.h"
+#include "ogl/glew.h"
 
 NS_VG_BEGIN
 
+class Shader;
+
+/// GLProgram is built with a vertex shader and a fragment shader
 class __declspec(dllexport) GLProgram
 {
+public:
+	GLProgram();
+	virtual ~GLProgram();
 
+	bool Build(const Shader* vertexShader, const Shader* fragmentShader);
+
+private:
+	/// Cleanup program object
+	/// Set _program value to 0
+	void DeleteProgram();
+
+private:
+	GLuint _program;
 };
 
 NS_VG_END
