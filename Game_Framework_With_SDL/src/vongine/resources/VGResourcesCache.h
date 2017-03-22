39a66e21308ff10de23cdace453bd1c7488ef2f5
Ignacio Pascual <vonflaken@gmail.com>
2017-03-22 01:07:23 +0100

    Add ResourceCache implementation. AddTexture function returns texture2d that matchs the filename provided, also creates a new texture if not exist yet.

--- Game_Framework_With_SDL/src/vongine/resources/VGResourcesCache.h
+++ Game_Framework_With_SDL/src/vongine/resources/VGResourcesCache.h
@@ -3,10 +3,11 @@
 
 #include "VGMacros.h"
 #include <unordered_map>
-#include "rendering/VGTexture2D.h"
 #include <memory>
 
 NS_VG_BEGIN
+
+class Texure2D;
 
 class __declspec(dllexport) ResourcesCache
 {
@@ -14,7 +15,7 @@ public:
 	/// Create new Texture2D and add it to the cache.
 	/// If already exists the texture with provided filename It will return the cached one.
 	/// Caller is not responsible of the ownership of the pointer.
-	const Texture2D* AddTexture(const std::string& filename);
+	const Texture2D* AddTexture(const char* filename);
 
 private:
 	std::unordered_map<std::string, std::unique_ptr<Texture2D>> _textures2D;
