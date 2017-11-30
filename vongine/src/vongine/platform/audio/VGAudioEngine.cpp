b4f29bef695186e1be24afed4e2d0f5937ef796a
Ignacio Pascual <vonflaken@gmail.com>
2017-11-30 04:38:33 +0100

    Forgot add this source to a previous commit.

---
+++ vongine/src/vongine/platform/audio/VGAudioEngine.cpp
@@ -0,0 +1,54 @@
+#include "VGAudioEngine.h"
+#include "base/VGTypes.h"
+#include "base/VGLogger.h"
+
+NS_VG_BEGIN
+
+AudioEngine::AudioEngine()
+: _fmodSystem(nullptr, VG_FMOD_Deleter())
+{}
+
+bool AudioEngine::Init()
+{
+	if (!_fmodSystem) // Is not initialized yet?
+	{
+		FMOD::System* sysPtr = _fmodSystem.get();
+		FMOD_RESULT res = FMOD::System_Create(&sysPtr); // Create a FMOD System object
+		VG_FMOD_ERRCHECK(res);
+
+		uint32 version;
+		res = _fmodSystem->getVersion(&version);
+		VG_FMOD_ERRCHECK(res);
+
+		if (version >= FMOD_VERSION)
+		{
+			return true;
+		}
+		else
+		{
+			VGLOG_ERROR("FMOD lib version %08x doesn't match header version %08x", version, FMOD_VERSION);
+
+			// Explicit clean resources
+			_fmodSystem.reset(nullptr);
+		}
+	}
+	return false;
+}
+
+std::unique_ptr<FMOD::Sound, VG_FMOD_Deleter> AudioEngine::CreateSound(const std::string& path, FMOD_MODE modeFlags, FMOD_CREATESOUNDEXINFO* exInfo) const
+{
+	if (_fmodSystem)
+	{
+		FMOD::Sound* sound;
+		FMOD_RESULT res = _fmodSystem->createSound(path.c_str(), modeFlags, exInfo, &sound);
+		VG_FMOD_ERRCHECK(res);
+
+		return std::unique_ptr<FMOD::Sound, VG_FMOD_Deleter>(sound, VG_FMOD_Deleter());
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+NS_VG_END
