9263208147f8e16a8425f58a57881e7f95606184
Ignacio Pascual <vonflaken@gmail.com>
2017-11-30 03:34:28 +0100

    - Add FMOD headers and bin files, link lib in project. - Add VGFMOD sources, include this instead of "fmod/fmod.hpp" directly. - Add VGAudioEngine sources, handles FMOD initialization, sound factory functions, playback, etc.

---
+++ vongine/src/vongine/platform/audio/VGAudioEngine.h
@@ -0,0 +1,28 @@
+#ifndef __VGAUDIOENGINE_H__
+#define __VGAUDIOENGINE_H__
+
+#include "VGMacros.h"
+
+#include "VGFMOD.h"
+
+#include <memory>
+#include <string>
+
+NS_VG_BEGIN
+
+class DLLAPI AudioEngine
+{
+public:
+	AudioEngine();
+
+	bool Init();
+
+	std::unique_ptr<FMOD::Sound, VG_FMOD_Deleter> CreateSound(const std::string& path, FMOD_MODE modeFlags, FMOD_CREATESOUNDEXINFO* exInfo) const;
+
+private:
+	std::unique_ptr<FMOD::System, VG_FMOD_Deleter> _fmodSystem;
+};
+
+NS_VG_END
+
+#endif // __VGAUDIOENGINE_H__
